43fb50644f64ed1e432c7bf424174f6e6fdadb5a
FreshGuy32 <mathias.trapl@gmail.com>
2025-12-11 19:35:38 +0000

    chore: rename some more blueprint stuff to NetzNÖ

--- custom_components/netznoe_hass/binary_sensor.py
+++ custom_components/netznoe_hass/binary_sensor.py
@@ -22,7 +22,7 @@ if TYPE_CHECKING:
 ENTITY_DESCRIPTIONS = (
     BinarySensorEntityDescription(
         key="netznoe_hass",
-        name="Integration Blueprint Binary Sensor",
+        name="NetzNÖ Binary Sensor",
         device_class=BinarySensorDeviceClass.CONNECTIVITY,
     ),
 )
